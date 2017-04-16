edc2907b6ba857627b05948b961703b4543d05b5
Aditya Gune <adi.vtca@gmail.com>
2017-04-16 01:35:10 -0700

    HW1 Part 1 working
    Homework 1 part 1 (FSA) done

--- assignment1/make.py
+++ assignment1/make.py
@@ -1,4 +1,26 @@
 """
 ASSIGNMENT 1
 """
+states = []
+f = open('vocab.small') #change to vocab for full dictionary
+t = open('english.fsa', 'w+')   #create fsa file
+#iterate thru every word in dict
+for w in f:
 
+    #iterate thru letter in word
+    for l in range(0, len(w)+1):
+        substr = w[:l]          #create substring
+        substr = substr[:-2]    #remove trailing whitespace and \n
+        #print (l, substr)
+
+        #do duplicate checking, handle base cases of empty/space string
+        if (substr not in states) and (substr != '') and (substr[-1] != ' '):
+
+            #build state string that can be written to the fsa
+            currentState = '(' + substr[:-1] + ' (' + substr + ', ' + substr[-1] + '))'
+            #print currentState
+            states.append(substr)
+
+            t.write(currentState+'\n')
+
+t.close()
