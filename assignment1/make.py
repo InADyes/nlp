ec5ecf7b4345f7d0d13e9c478ebdbfdc6bf7fddd
agune001 <ca_adi@yahoo.com>
2017-04-16 13:04:12 -0700

    Added transitions, made pipeable in bash
    -Added epsilon transition ([word] (1 *e*))
    -Added final state transition (1 (0 _))
    -Made pipeable in Bash per specified input requirements

--- assignment1/make.py
+++ assignment1/make.py
@@ -1,26 +1,31 @@
 """
 ASSIGNMENT 1
 """
+import sys
+
 states = []
-f = open('vocab.small') #change to vocab for full dictionary
+#f = open('vocab.small') #used for testing purposes
 t = open('english.fsa', 'w+')   #create fsa file
 #iterate thru every word in dict
-for w in f:
+for w in sys.stdin:
 
     #iterate thru letter in word
     for l in range(0, len(w)+1):
         substr = w[:l]          #create substring
         substr = substr[:-2]    #remove trailing whitespace and \n
-        #print (l, substr)
+        substr = substr.replace(" ", "")
 
         #do duplicate checking, handle base cases of empty/space string
         if (substr not in states) and (substr != '') and (substr[-1] != ' '):
 
             #build state string that can be written to the fsa
-            currentState = '(' + substr[:-1] + ' (' + substr + ', ' + substr[-1] + '))'
+            currentState = '(' + substr[:-1] + ' (' + substr + ' ' + substr[-1] + '))'
             #print currentState
             states.append(substr)
-
+            if currentState == "( (A A))":
+                currentState = "1\n(0 (A A))"
             t.write(currentState+'\n')
 
+    t.write('(' + substr + ' (1 *e*))\n')
+t.write('(1 (0 _))\n')
 t.close()
