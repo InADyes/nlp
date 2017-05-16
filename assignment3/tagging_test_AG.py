b8e8f019e9578b28abdd26e51ed4a1fd423b645d
agune001 <ca_adi@yahoo.com>
2017-05-15 20:45:40 -0700

    loop working correctly

--- assignment3/tagging_test_AG.py
+++ assignment3/tagging_test_AG.py
@@ -8,19 +8,21 @@ Weight matrix:
 def getprobabilities():
     wt = defaultdict(float)
     regex = re.compile('[^a-zA-Z0-9]')
-    
+    tag_dict = defaultdict(list)
     lexicon = open('lexicon-w-t.txt', 'r')
     for line in lexicon:
         x = regex.sub(' ', line)
         x = x.split()
+        #print(x)
         if len(x) < 1:
-            return wt
+            return (wt, tag_dict)
         ptuple = (x[1], x[0])
+        tag_dict[x[1]].append(x[0])
         if len(x[-1]) > 1:
             wt[ptuple] = int(x[-1])/100
         else:
             wt[ptuple] = int(x[-1])/10
-    return wt
+    return (wt, tag_dict)
         
 def getWeights(tags):
     file = open('bigram.wfsa', 'r')
@@ -57,13 +59,13 @@ def viterbi(line, wt, T, tags):
     back = []
     opt[0] = 1
     for i in range(len(line)):
-        for tag in range(len(tags)):
-            for tag_prev in range(len(tags)):
-                
-                
+        for tag in tag_dict[line[i]]:   #tag = tag of word i
+            for tag_prev in tag_dict[line[i-1]]:    #t_prev = tag of i - 1
+                                
                 ###ERROR IS PROBABLY HERE####
-                ptuple = (line[i], tags[tag])
-                ptt = T[tag_prev][tag] #ptt = p(tag | tag_prev)
+                ptuple = (line[i], tag)
+                #print(ptuple)
+                ptt = T[tags.index(tag_prev)][tags.index(tag)] #ptt = p(tag | tag_prev)
                 ptw = wt[ptuple] #ptw = p(word | tag)
                 p = ptt * ptw #p = p(tag | tag_prev) * p(word | tag)
                 mu = opt[i-1] * p #prob of best seq ending in i - 1
@@ -71,13 +73,14 @@ def viterbi(line, wt, T, tags):
                 if mu > opt[i]:
                     opt[i] = mu
                     back.append(tag)
-                #############################
                 
+                #############################
+    
+    del back[-1]             
     tag_seq = [0]* len(line)
-    tag_seq[-1] = "***"
     for i in range(len(line), 0, -1):
-        #print(back)
-        tag_seq[i-1] = tags[back[i]]
+
+        tag_seq[i-1] = back[i-1]
     return tag_seq
 
 
@@ -86,7 +89,7 @@ def viterbi(line, wt, T, tags):
 tags = []
 
 (E,T, tags) = getWeights(tags)
-wt = getprobabilities()
+(wt, tag_dict) = getprobabilities()
 line = "They can fish"#"The panda eats shoots and leaves"
 results = viterbi(line, wt, T, tags)
 print(results)
