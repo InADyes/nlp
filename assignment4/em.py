80f61565256d60e39db392afcb36f42d773fd36e
Aditya Gune <adi.vtca@gmail.com>
2017-05-29 13:50:46 -0700

    em.py
    Enumerate counts in epron-jpron alignments and their counts and
    probabilities for em.py

--- assignment4/em.py
+++ assignment4/em.py
@@ -14,6 +14,7 @@ file = open('epron-jpron.data.test', 'r')
 def isnumeric(string):
    return bool(re.search(r'\d', string))
 
+counts = defaultdict(lambda: defaultdict(float))
 probs = defaultdict(lambda: defaultdict(float))
 epjp = []
 
@@ -21,42 +22,50 @@ def getAlignments(epron, jpron):
     
     #handle base case
     if len(epron) == 1:
-        alignment = [[''.join(epron), ''.join(jpron)]]
+        alignment = [[(''.join(epron), ''.join(jpron))]]
         return alignment
     
+    #recursive case
     alignments = []
     for i in range(min(3, len(jpron), len(epron))):
         curr_j = ' '.join(jpron[:i + 1])
         align = getAlignments(epron[1:],jpron[i+1:])
-        alignments += [[(epron[0], curr_j)] + r for r in align]
+        for r in align:
+            alignments += [[(epron[0], curr_j)] + r]
     return alignments
         
-
+#process input
 while True:
     lines = islice(file, 3)
 
     stack =[]
+    
     #create the stack
     for line in lines:
         stack.append(line)
     if stack == []:break
-    epron = stack[0].split()#re.split(r'(\s+)', stack[0])
-    jpron = stack[1].split()# re.split(r'(\s+)', stack[1])
-    
+    epron = stack[0].split()
+    jpron = stack[1].split()
     
     epjp.append((epron, jpron))
-    
+
+alignments = []
+
+#iterate thru every pairing to generate alignments
 for pair in epjp:
     epron = pair[0]
     jpron = pair[1]
     al = getAlignments(epron, jpron)
-    print (al)
+   
     for a in al:
-        print(a)
         for pair in a:
-            x = 1
-            del x
-            #probs[pair[0]][pair[1]] += 1
-    
-
-    
+            
+            #only enumerate legal alignments
+            #eliminates alignments like ("ER", "")
+            #where epron does not map to any jpron
+            if '' not in pair:  
+                counts[pair[0]][pair[1]] += 1
+                      
+for epron in counts.keys():
+    for jpron in counts[epron].keys():
+        probs[epron][jpron] = counts[epron][jpron] / sum([i for i in counts[epron].values()])
