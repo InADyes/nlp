0ff55dffd26474f7274916d3a05009f803738b6d
Aditya Gune <adi.vtca@gmail.com>
2017-05-29 17:36:38 -0700

    em.py
    em.py written

--- assignment4/em.py
+++ assignment4/em.py
@@ -1,22 +1,24 @@
-# -*- coding: utf-8 -*-
+#!/usr/bin/python3
 """
 ASSIGNMENT 4 NLP
 @author: Aditya
 """
 
-import numpy as np
 import re
 from collections import defaultdict
 from itertools import *
+import sys
 
-file = open('epron-jpron.data.test', 'r')
+#file = open('epron-jpron.data','r')
+
+
+epjp = []
+inputStrings = []
+iterations = int(sys.argv[1])
+
 
 def isnumeric(string):
    return bool(re.search(r'\d', string))
-
-counts = defaultdict(lambda: defaultdict(float))
-probs = defaultdict(lambda: defaultdict(float))
-epjp = []
 
 def getAlignments(epron, jpron):
     
@@ -33,23 +35,28 @@ def getAlignments(epron, jpron):
         for r in align:
             alignments += [[(epron[0], curr_j)] + r]
     return alignments
-        
-#process input
-while True:
-    lines = islice(file, 3)
 
-    stack =[]
-    
-    #create the stack
-    for line in lines:
-        stack.append(line)
-    if stack == []:break
-    epron = stack[0].split()
-    jpron = stack[1].split()
-    
+#process input
+for line in sys.stdin:
+    line = line.split("\n")
+    if not isnumeric(line[0]):
+        inputStrings.append(line[0])
+    #print(line)
+
+for i in range(0, len(inputStrings)-2, 2): 
+    estring = inputStrings[i]
+    jstring = inputStrings[i+1]
+
+    epron = estring.split()
+    jpron = jstring.split()
+    #print(epron, jpron)
     epjp.append((epron, jpron))
 
+
+
 alignments = []
+counts = defaultdict(lambda: defaultdict(float))
+pr_prior = defaultdict(lambda: defaultdict(float))
 
 #iterate thru every pairing to generate alignments
 for pair in epjp:
@@ -58,14 +65,61 @@ for pair in epjp:
     al = getAlignments(epron, jpron)
    
     for a in al:
-        for pair in a:
+        for tpair in a:
             
             #only enumerate legal alignments
             #eliminates alignments like ("ER", "")
             #where epron does not map to any jpron
-            if '' not in pair:  
-                counts[pair[0]][pair[1]] += 1
-                      
+            if '' not in tpair:  
+                counts[tpair[0]][tpair[1]] += 1
+       
+#initialize the probabilities               
 for epron in counts.keys():
     for jpron in counts[epron].keys():
-        probs[epron][jpron] = counts[epron][jpron] / sum([i for i in counts[epron].values()])
+        count = sum([i for i in counts[epron].values()])
+        pr_prior[epron][jpron] = counts[epron][jpron] / count
+             
+
+nonzero = 0
+for epron in pr_prior.keys():
+    for jpron in pr_prior[epron].keys():
+        p_EJ = round(pr_prior[epron][jpron], 3)
+        if p_EJ > 0.01:
+            nonzero += 1
+        #print(epron, " : ", jpron, " # ", p_EJ)
+
+for i in range(iterations):
+
+    p = 0
+    total = 0
+    probdict =  defaultdict(lambda: defaultdict(float))
+    
+    for pair in epjp:
+        epron = pair[0]
+        jpron = pair[1]
+        alignments = getAlignments(epron, jpron)
+        tarray = []
+        for a in alignments:
+            p_accum = 1
+            
+            for tpair in a:
+                p_accum *= pr_prior[tpair[0]][tpair[1]]
+            
+            tarray.append(p_accum)
+            total += p_accum
+        tarray = [p / sum(tarray) for p in tarray]
+        
+        for x, j in enumerate(alignments):
+            for k in j:
+                probdict[k[0]][k[1]] += tarray[x]
+
+        print("iteration ",i," ----- corpus prob= ",total)
+        for epron in pr_prior.keys():
+            for jpron in pr_prior[epron].keys():
+                p_EJ = round(pr_prior[epron][jpron], 3)
+                if p_EJ > 0.01:
+                    nonzero += 1
+                print(epron, " : ", jpron, " # ", p_EJ)
+        print("Nonzero = ", nonzero)
+        nonzero = 0
+        pr_prior = probdict
