efd7a14c7d02c6e4c61e37edd39e6e4d72460917
InADyes <liuhao0320@gmail.com>
2017-06-01 14:42:22 -0700

    bad replacement

---
+++ assignment5/replace_onecounts.py
@@ -0,0 +1,25 @@
+from collections import defaultdict
+
+f = open('train.trees.space','r')
+cntdict = defaultdict(dict)
+
+for line in f:
+    s = line.split()
+    for i in range(0, len(s)):
+	#print s[i]
+	if s[i] not in cntdict:
+	    cntdict[s[i]] = 1
+	else:
+	    cntdict[s[i]] += 1
+f.close()
+f = open('train.trees.space','r')
+for lines in f:
+    k = lines.split() 
+    for n,i in enumerate(k):
+	if cntdict[i] == 1:
+	    k[n] = '<unk>'
+	    #lines.replace(old,'<unk>')
+	    #print k[b]
+    str1 = ''.join(k)
+    print str1
+f.close()
